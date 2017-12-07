743b5fb311015338b8db7c854b69e1c1726e6a45
Brett Hartman <brett.hartman@mac.com>
2017-12-06 19:10:07 -0800

    Add user and stock association plus views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
@@ -11,5 +14,9 @@ class Stock < ApplicationRecord
 
   def self.strip_commas(number)
     number.delete(',')
+  end
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
